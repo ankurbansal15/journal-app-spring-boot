eb40ff7774b4d1394c39ec73c795449ebe01038e
ankurbansal15 <ankurbansal153@gmail.com>
2025-07-09 07:49:18 +0530

    RESTful Api's Learning

---
+++ src/test/java/in/ankurbansal/journalApp/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ankurbansal.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
